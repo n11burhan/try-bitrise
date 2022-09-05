41a02c7900201c7210298a7ca927ef18a0fed02d
burhan.karakurt <burhan.karakurt@n11.com>
2022-09-05 09:18:16 +0300

    build

--- app/src/test/java/com/example/trybitrice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/trybitrice/ExampleUnitTest.kt
@@ -12,7 +12,7 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(5, 3 + 2)
+        assertEquals(6, 4 + 2)
     }
     @Test
     fun addition_isUnCorrect() {
